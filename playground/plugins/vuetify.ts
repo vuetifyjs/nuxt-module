dd56be57e3403830c80cd73762b1317429671945
userquin <userquin@gmail.com>
2023-07-01 17:58:33 +0200

    chore: include vuetify in types and update plugin hook signature

--- playground/plugins/vuetify.ts
+++ playground/plugins/vuetify.ts
@@ -1,6 +1,8 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('vuetify:configuration', (vuetifyOptions) => {
-    // eslint-disable-next-line no-console
-    console.log('vuetify:plugin:hook', vuetifyOptions)
+  nuxtApp.hook('vuetify:configuration', (isDev, vuetifyOptions) => {
+    if (process.client && isDev) {
+      // eslint-disable-next-line no-console
+      console.log('vuetify:plugin:hook', vuetifyOptions)
+    }
   })
 })
