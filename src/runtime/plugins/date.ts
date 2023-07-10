78ca44d873129588881ca852c542d3cd0eab20b3
userquin <userquin@gmail.com>
2023-07-10 16:33:27 +0200

    fix: update nuxt plugins logic

--- src/runtime/plugins/date.ts
+++ src/runtime/plugins/date.ts
@@ -1,6 +1,7 @@
 import { adapter, dateConfiguration, i18n } from 'virtual:vuetify-date-configuration'
 import type { VuetifyOptions } from 'vuetify'
 import { useNuxtApp } from '#app'
+import type { LocaleObject } from '#i18n'
 
 export function configureDate(vuetifyOptions: VuetifyOptions) {
   if (adapter === 'custom')
@@ -9,12 +10,12 @@ export function configureDate(vuetifyOptions: VuetifyOptions) {
   const dateOptions = dateConfiguration()
 
   if (i18n) {
-    const locales = useNuxtApp().$i18n.locales.value
+    const locales: LocaleObject[] | undefined = useNuxtApp().$i18n.locales.value
     if (locales) {
       dateOptions.locale = locales.reduce((acc, locale) => {
         acc[locale.code] = locale.code
         return acc
-      }, {})
+      }, <Record<string, any>>{})
     }
   }
 
