de10af9b15105ee27382c7b6c69ab36187def6a2
Joaquín Sánchez <userquin@gmail.com>
2023-09-10 13:00:42 +0200

    docs: bump to VitePress v1.0.0-rc11 (#94)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import Theme from 'vitepress/theme'
 import { inBrowser } from 'vitepress'
+import VuetifyLayout from './VuetifyLayout.vue'
 
 import './styles/main.css'
 import './styles/vars.css'
@@ -9,4 +10,7 @@ import 'uno.css'
 if (inBrowser)
   import('./pwa')
 
-export default Theme
+export default {
+  ...Theme,
+  Layout: VuetifyLayout,
+}
