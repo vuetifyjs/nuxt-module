a83494aca37230207cfd5a182d9c17f81b72661b
userquin <userquin@gmail.com>
2023-11-06 12:57:52 +0100

    fix: add missing imports in client hints

--- src/runtime/plugins/config.ts
+++ src/runtime/plugins/config.ts
@@ -12,12 +12,14 @@ export async function configureVuetify() {
 
   const vuetify = createVuetify(vuetifyOptions)
 
+  // @ts-expect-error Vuetify using App<any> instead of App<Element>
   nuxtApp.vueApp.use(vuetify)
 
   nuxtApp.provide('vuetify', vuetify)
 
   await nuxtApp.hooks.callHook('vuetify:ready', vuetify)
 
+  // eslint-disable-next-line n/prefer-global/process
   if (process.client) {
     // eslint-disable-next-line no-console
     isDev && console.log('Vuetify 3 initialized', vuetify)
