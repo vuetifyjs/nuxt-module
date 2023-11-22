6d12e1435de66d0a6a0d2d630fd47b244b6bc575
userquin <userquin@gmail.com>
2023-11-22 22:02:08 +0100

    chore: update module hook in playground

--- playground/layer-module/index.ts
+++ playground/layer-module/index.ts
@@ -2,7 +2,7 @@ import { defineNuxtModule } from '@nuxt/kit'
 
 export default defineNuxtModule({
   setup(_options, nuxt) {
-    nuxt.hook('vuetify:registerModule', (register: any) => register({
+    nuxt.hook('vuetify:registerModule', registerModule => registerModule({
       vuetifyOptions: {
         aliases: {
           MyBtn: 'VBtn',
