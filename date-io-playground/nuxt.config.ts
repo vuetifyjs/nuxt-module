3dee85ff0aa92b3f320ae728b63e2b0054cc90f1
userquin <userquin@gmail.com>
2023-12-07 15:54:53 +0100

    chore: add jalali

--- date-io-playground/nuxt.config.ts
+++ date-io-playground/nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
     },
     vuetifyOptions: {
       date: {
-        adapter: 'date-fns-jalali',
+        adapter: 'jalaali',
       },
       locale: {
         locale: 'es',
