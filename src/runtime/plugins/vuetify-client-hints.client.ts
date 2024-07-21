b80851d125cdd6050e0575fcc89f718e9ed65dca
Joaquín Sánchez <userquin@gmail.com>
2024-07-21 10:09:07 +0200

    fix(ssr)!: include device pixel ratio in w/h calculation (#199)
    * fix: change SSR client hints server plugin order
    
    * chore: include device pixel ratio logic
    
    * chore: set vuetify ssr to true

--- src/runtime/plugins/vuetify-client-hints.client.ts
+++ src/runtime/plugins/vuetify-client-hints.client.ts
@@ -60,56 +60,57 @@ const plugin: Plugin<{
         window.location.reload()
     }
 
-    if (viewportSize || (prefersColorScheme && prefersColorSchemeOptions)) {
-      // restore SSR state
-      nuxtApp.hook('vuetify:before-create', ({ vuetifyOptions }) => {
-        // on client, we update the display to avoid hydration mismatch on page refresh
-        // there will be some hydration mismatch since the headers sent by the user agent may not be accurate
-        if (viewportSize) {
-          const clientWidth = state.value.viewportWidth
-          const clientHeight = state.value.viewportHeight
-          vuetifyOptions.ssr = typeof clientWidth === 'number'
-            ? {
-                clientWidth,
-                clientHeight,
-              }
-            : true
-        }
-
-        // update the theme
-        if (prefersColorScheme && prefersColorSchemeOptions) {
-          if (vuetifyOptions.theme === false) {
-            vuetifyOptions.theme = { defaultTheme: state.value.colorSchemeFromCookie ?? prefersColorSchemeOptions.defaultTheme }
-          }
-          else {
-            vuetifyOptions.theme = vuetifyOptions.theme ?? {}
-            vuetifyOptions.theme.defaultTheme = state.value.colorSchemeFromCookie ?? prefersColorSchemeOptions.defaultTheme
-          }
-        }
-      })
-
-      // update theme logic
-      if (prefersColorScheme && prefersColorSchemeOptions) {
-        const themeCookie = state.value.colorSchemeCookie
-        if (themeCookie) {
-          nuxtApp.hook('app:beforeMount', () => {
-            const vuetify = useNuxtApp().$vuetify
-            // update the theme
-            const cookieName = prefersColorSchemeOptions.cookieName
-            const parseCookieName = `${cookieName}=`
-            const cookieEntry = `${parseCookieName}${state.value.colorSchemeFromCookie ?? prefersColorSchemeOptions.defaultTheme};`
-            watch(vuetify.theme.global.name, (newThemeName) => {
-              document.cookie = themeCookie.replace(cookieEntry, `${cookieName}=${newThemeName};`)
-            })
-            if (prefersColorSchemeOptions.useBrowserThemeOnly) {
-              const { darkThemeName, lightThemeName } = prefersColorSchemeOptions
-              const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
-              prefersDark.addEventListener('change', (e) => {
-                vuetify.theme.global.name.value = e.matches ? darkThemeName : lightThemeName
-              })
+    // restore SSR state
+    nuxtApp.hook('vuetify:before-create', ({ vuetifyOptions }) => {
+      // on client, we update the display to avoid hydration mismatch on page refresh
+      // there will be some hydration mismatch since the headers sent by the user agent may not be accurate
+      if (viewportSize) {
+        const clientWidth = state.value.viewportWidth
+        const clientHeight = state.value.viewportHeight
+        vuetifyOptions.ssr = typeof clientWidth === 'number'
+          ? {
+              clientWidth,
+              clientHeight,
             }
-          })
+          : true
+      }
+      else {
+        vuetifyOptions.ssr = true
+      }
+
+      // update the theme
+      if (prefersColorScheme && prefersColorSchemeOptions) {
+        if (vuetifyOptions.theme === false) {
+          vuetifyOptions.theme = { defaultTheme: state.value.colorSchemeFromCookie ?? prefersColorSchemeOptions.defaultTheme }
         }
+        else {
+          vuetifyOptions.theme = vuetifyOptions.theme ?? {}
+          vuetifyOptions.theme.defaultTheme = state.value.colorSchemeFromCookie ?? prefersColorSchemeOptions.defaultTheme
+        }
+      }
+    })
+
+    // update theme logic
+    if (prefersColorScheme && prefersColorSchemeOptions) {
+      const themeCookie = state.value.colorSchemeCookie
+      if (themeCookie) {
+        nuxtApp.hook('app:beforeMount', () => {
+          const vuetify = useNuxtApp().$vuetify
+          // update the theme
+          const cookieName = prefersColorSchemeOptions.cookieName
+          const parseCookieName = `${cookieName}=`
+          const cookieEntry = `${parseCookieName}${state.value.colorSchemeFromCookie ?? prefersColorSchemeOptions.defaultTheme};`
+          watch(vuetify.theme.global.name, (newThemeName) => {
+            document.cookie = themeCookie.replace(cookieEntry, `${cookieName}=${newThemeName};`)
+          })
+          if (prefersColorSchemeOptions.useBrowserThemeOnly) {
+            const { darkThemeName, lightThemeName } = prefersColorSchemeOptions
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
+            prefersDark.addEventListener('change', (e) => {
+              vuetify.theme.global.name.value = e.matches ? darkThemeName : lightThemeName
+            })
+          }
+        })
       }
     }
 
