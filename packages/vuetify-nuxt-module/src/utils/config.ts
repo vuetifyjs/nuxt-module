6fa58c57638ad5e4303e86e0450b3af535c27c56
Andrei Elkin <andreyyolkin@gmail.com>
2026-04-19 20:38:14 +0200

    refactor(styles): remove stylesCachePath from context

--- packages/vuetify-nuxt-module/src/utils/config.ts
+++ packages/vuetify-nuxt-module/src/utils/config.ts
@@ -34,7 +34,6 @@ export interface VuetifyNuxtContext {
   viteVersion: string
   enableRules?: boolean
   rulesConfiguration?: { fromLabs?: boolean, configFile?: string }
-  stylesCachePath?: string
   stylesConfigFile?: string
 }
 
