3db3133f41c40d3f5aa58894cedcce46d63912c5
Andrei Elkin <andreyyolkin@gmail.com>
2026-04-21 22:56:08 +0200

    test(styles): minimal SSR fixture for unplugin-styles e2e

---
+++ packages/vuetify-nuxt-module/test/fixtures/styles-ssr/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app>
+    <NuxtPage />
+  </v-app>
+</template>
---
+++ packages/vuetify-nuxt-module/test/fixtures/styles-ssr/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container id="smoke">
+    <v-row>
+      <v-col cols="12">
+        <p id="body-font-sample">Body font sample</p>
+        <v-btn id="smoke-btn">Hi</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
